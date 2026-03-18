25c0a4fc39a46fbb60e8507ba16cd3ac94cb7965
Lppetrov0 <demirando8@abv.bg>
2026-03-18 13:22:29 +0200

    Jwt token generation

--- backend/user-service/src/main/java/com/chist/userservice/security/JwtService.java
+++ backend/user-service/src/main/java/com/chist/userservice/security/JwtService.java
@@ -1,8 +1,35 @@
 package com.chist.userservice.security;
 
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
 
 @Service
 public class JwtService {
+
+    @Value("{jwt.secret}")
+    private String secretKey;
+
+    @Value("{jwt.expiration}")
+    private long expirationTime;
+
+    private Key getSignInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String email){
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(getSignInKey())
+                .compact();
+    }
 }
