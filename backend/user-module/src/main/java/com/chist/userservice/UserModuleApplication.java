f2a096ff70ea5b28d63ed279e8c42dc2686092c6
Lppetrov0 <demirando8@abv.bg>
2026-03-23 15:23:36 +0200

    changing role names

--- backend/user-module/src/main/java/com/chist/userservice/UserModuleApplication.java
+++ backend/user-module/src/main/java/com/chist/userservice/UserModuleApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class UserServiceApplication {
+public class UserModuleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(UserModuleApplication.class, args);
     }
 
 }
