533b63dfb797edca73ffdfcda2fcf049fca7d1cd
Lppetrov0 <demirando8@abv.bg>
2026-03-26 22:18:53 +0200

    Created Global Exception Handler

---
+++ backend/user-module/src/main/java/com/chist/userservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.chist.userservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String>handleUserNotFound(UserNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Invalid Credentials.");
+    }
+
+    @ExceptionHandler(EmailAlreadyExistException.class)
+    public ResponseEntity<String>handleEmailExists(EmailAlreadyExistException ex){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("User already exists.");
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String>handleRuntime(RuntimeException ex){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong.");
+    }
+}
