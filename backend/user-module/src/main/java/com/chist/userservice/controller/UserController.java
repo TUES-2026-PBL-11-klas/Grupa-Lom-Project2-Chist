234e2bbe72438a90cad875dc72ad6d654c6c2b73
Lppetrov0 <demirando8@abv.bg>
2026-03-26 21:51:07 +0200

    finished usercontroller

--- backend/user-module/src/main/java/com/chist/userservice/controller/UserController.java
+++ backend/user-module/src/main/java/com/chist/userservice/controller/UserController.java
@@ -1,4 +1,33 @@
 package com.chist.userservice.controller;
 
+
+import com.chist.userservice.model.User;
+import com.chist.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserRepository userRepository;
+
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getCurrentUser(@AuthenticationPrincipal User user){
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable UUID id){
+        return userRepository.findById(id).map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
 }
