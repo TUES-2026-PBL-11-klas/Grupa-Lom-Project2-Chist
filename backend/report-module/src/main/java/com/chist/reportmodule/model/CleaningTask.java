7b76c66b66c856eef87addecdd4717d64fe1f054
Lppetrov0 <demirando8@abv.bg>
2026-03-27 22:44:56 +0200

    Task status enum and cleaning task model completion

--- backend/report-module/src/main/java/com/chist/reportmodule/model/CleaningTask.java
+++ backend/report-module/src/main/java/com/chist/reportmodule/model/CleaningTask.java
@@ -3,8 +3,11 @@ package com.chist.reportmodule.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.util.Date;
 import java.util.UUID;
 
 
@@ -23,5 +26,29 @@ public class CleaningTask {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "report_id",nullable = false)
     private Report report;
+
+    @Column(nullable = false)
+    private UUID cleanerId;
+
+    @Column(name = "before_photo")
+    private String beforePhoto;
+
+    @Column(name = "after_photo")
+    private String afterPhoto;
+
+    @Column(nullable = false)
+    private boolean verified = false;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskStatus status = TaskStatus.PENDING;
+
+    @CreatedDate
+    @Column(nullable = false, name = "created_at")
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false, name = "updated_at")
+    private Date updatedAt;
 
 }
