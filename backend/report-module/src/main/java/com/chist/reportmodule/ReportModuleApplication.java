8fdd27e720ed7c465978ec519c5d9f9ca20f4744
Lppetrov0 <demirando8@abv.bg>
2026-03-27 22:21:51 +0200

    Created Reports model

--- backend/report-module/src/main/java/com/chist/reportmodule/ReportModuleApplication.java
+++ backend/report-module/src/main/java/com/chist/reportmodule/ReportModuleApplication.java
@@ -2,8 +2,10 @@ package com.chist.reportmodule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ReportModuleApplication {
 
     public static void main(String[] args) {
