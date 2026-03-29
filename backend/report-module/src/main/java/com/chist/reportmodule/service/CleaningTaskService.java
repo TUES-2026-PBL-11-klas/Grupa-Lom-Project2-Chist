9fbecad5176e05da8cc54d601b606abdf419510e
Lppetrov0 <demirando8@abv.bg>
2026-03-29 19:57:09 +0300

    Creating cleaning taks service

---
+++ backend/report-module/src/main/java/com/chist/reportmodule/service/CleaningTaskService.java
@@ -0,0 +1,43 @@
+package com.chist.reportmodule.service;
+
+import com.chist.reportmodule.dto.CleaningTaskResponse;
+import com.chist.reportmodule.dto.CreateCleaningTaskRequest;
+import com.chist.reportmodule.exception.ReportNotFoundException;
+import com.chist.reportmodule.model.CleaningTask;
+import com.chist.reportmodule.model.Report;
+import com.chist.reportmodule.model.ReportStatus;
+import com.chist.reportmodule.model.TaskStatus;
+import com.chist.reportmodule.repository.CleaningTaskRepository;
+import com.chist.reportmodule.repository.ReportRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CleaningTaskService {
+
+    private final CleaningTaskRepository cleaningTaskRepository;
+    private final ReportRepository reportRepository;
+
+    public CleaningTaskResponse createCleaningTask(UUID cleanerId, CreateCleaningTaskRequest request){
+        Report report = reportRepository.findById(request.getReportId())
+                .orElseThrow(() -> new ReportNotFoundException("Report Not Found."));
+        report.setStatus(ReportStatus.IN_PROGRESS);
+        reportRepository.save(report);
+
+        CleaningTask cleaningTask = CleaningTask.builder()
+                .report(report)
+                .cleanerId(cleanerId)
+                .status(TaskStatus.PENDING)
+                .verified(false)
+                .build();
+
+        return //Dto map
+    }
+
+
+
+
+}
