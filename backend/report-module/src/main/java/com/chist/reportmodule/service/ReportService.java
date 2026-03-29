ea90d29096eb2f73065e5c98ab90295dad3e6f37
Lppetrov0 <demirando8@abv.bg>
2026-03-29 19:21:32 +0300

    New report service methods

--- backend/report-module/src/main/java/com/chist/reportmodule/service/ReportService.java
+++ backend/report-module/src/main/java/com/chist/reportmodule/service/ReportService.java
@@ -9,7 +9,9 @@ import com.chist.reportmodule.repository.ReportRepository;
 import lombok.*;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -26,6 +28,62 @@ public class ReportService {
                 .description(request.getDescription())
                 .status(ReportStatus.NEW)
                 .build();
-        return
+        return mapToDTO(reportRepository.save(report));
+
+    }
+
+    public ReportResponse getReportById(UUID  reportId){
+        return mapToDTO(reportRepository.findById(reportId)
+                .orElseThrow(() -> new /*Custom report*/));
+    }
+
+    public List<ReportResponse> getReportsByUserId(UUID userId){
+        return reportRepository.findByUserId(userId)
+                .stream()
+                .map(this :: mapToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public List<ReportResponse> getReportsByStatus(ReportStatus status){
+        return reportRepository.findByStatus(status)
+                .stream()
+                .map(this :: mapToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public List<ReportResponse> getAllReports(){
+        return reportRepository.findAll()
+                .stream()
+                .map(this :: mapToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public ReportResponse updateStatus(UUID reportId, ReportStatus status){
+        Report report = reportRepository.findById(reportId)
+                .orElseThrow(() -> new /*Custom exception*/);
+        report.setStatus(status);
+        return mapToDTO(reportRepository.save(report));
+    }
+
+    public void deleteReport(UUID reportId){
+        if(!reportRepository.existsById(reportId)){
+            throw new /*Custom exception*/
+        }
+        reportRepository.deleteById(reportId);
+    }
+
+
+    private ReportResponse mapToDTO(Report report) {
+        return ReportResponse.builder()
+                .reportId(report.getId())
+                .userId(report.getUserId())
+                .latitude(report.getLatitude())
+                .longitude(report.getLongitude())
+                .photoUrl(report.getPhotoUrl())
+                .description(report.getDescription())
+                .status(report.getStatus())
+                .createdAt(report.getCreatedAt())
+                .updatedAt(report.getUpdatedAt())
+                .build();
     }
 }
